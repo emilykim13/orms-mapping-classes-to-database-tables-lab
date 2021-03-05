167b75cf8aaa98473eeba8d5a02e007e7bce46f1
github username <github email address>
2021-03-05 14:03:32 -0600

    commit tree cb3b5ce5a96f73ef18fbaf44773df6b79c5ef401

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,49 @@
 class Student
-
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
+  attr_accessor :name, :grade
+  attr_reader :id
   
+  def initialize(name, grade, id=nil) 
+    # set id=nil because we do not set that student's id; they will get an id when they get saved into the database; 
+    # therefore we're able to create new student instances that do not have an id value (yet). we let the db handle it for organizational purposes
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+      SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students;
+      SQL
+      DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?,?);
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten[0]  # .flatten[0] = [0][0]
+      # we need an instance of @id
+  end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
 end
